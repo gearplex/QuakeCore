a4e789892dde14402d643e7fa2ddfed30d34d6e3
gearplex <garrett.r.hagen@gmail.com>
2026-09-13 19:45:49 -0700

    Gate4: expose ConcreteCM legacy trial for rule77 wrapper

--- include/quake/concrete_cm.hpp
+++ include/quake/concrete_cm.hpp
@@ -109,9 +109,14 @@ public:
     const ConcreteCMParameters& parameters() const noexcept { return envelope_.parameters(); }
     ConcreteCMState initial_state() const noexcept;
     ConcreteCMTrial trial(double strain, const ConcreteCMState& committed) const;
+    ConcreteCMTrial trial_legacy(double strain, const ConcreteCMState& committed) const;
 
 private:
     ConcreteCMEnvelope envelope_;
 };
 
 } // namespace quake
+
+#ifdef QUAKE_CONCRETE_CM_RENAME_TRIAL
+#define trial trial_legacy
+#endif
