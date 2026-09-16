c46bc17e8fd937d89598d62c3bce36f08afa9900
gearplex <garrett.r.hagen@gmail.com>
2026-09-15 18:04:23 -0700

    Gate4: parameterize ConcreteCM state tracer

--- validation/yori_wall_10001/concrete_cm_state_trace.cpp
+++ validation/yori_wall_10001/concrete_cm_state_trace.cpp
@@ -1,5 +1,6 @@
 #include "quake/concrete_cm.hpp"
 
+#include <cstdlib>
 #include <fstream>
 #include <iomanip>
 #include <iostream>
@@ -30,8 +31,9 @@ static void dump(std::ostream& out, int i, const ConcreteCMState& s) {
 }
 
 int main(int argc, char** argv) {
-    if (argc != 3) {
-        std::cerr << "usage: concrete_cm_state_trace strains.txt trace.csv\n";
+    if (argc != 3 && argc != 13) {
+        std::cerr << "usage: concrete_cm_state_trace strains.txt trace.csv "
+                     "[fc epsc Ec rc xcrn ft et rt xcrp gap_close]\n";
         return 2;
     }
     std::ifstream in(argv[1]);
@@ -43,16 +45,31 @@ int main(int argc, char** argv) {
     if (strains.empty()) return 2;
 
     ConcreteCMParameters p;
-    p.fc = -7.17649;
-    p.epsc = -0.00304075;
-    p.Ec = 4828.707488552191;
-    p.rc = 7.0;
-    p.xcrn = 1.015;
-    p.ft = 0.0635356;
-    p.et = 2.631577918133538e-05;
-    p.rt = 1.2;
-    p.xcrp = 10000.0;
-    p.gap_close = true;
+    if (argc == 13) {
+        p.fc = std::stod(argv[3]);
+        p.epsc = std::stod(argv[4]);
+        p.Ec = std::stod(argv[5]);
+        p.rc = std::stod(argv[6]);
+        p.xcrn = std::stod(argv[7]);
+        p.ft = std::stod(argv[8]);
+        p.et = std::stod(argv[9]);
+        p.rt = std::stod(argv[10]);
+        p.xcrp = std::stod(argv[11]);
+        p.gap_close = std::stoi(argv[12]) != 0;
+    } else {
+        // Legacy default: story-3 confined concrete used by the original
+        // wall-5/fiber-0 tangent diagnostic.
+        p.fc = -7.17649;
+        p.epsc = -0.00304075;
+        p.Ec = 4828.707488552191;
+        p.rc = 7.0;
+        p.xcrn = 1.015;
+        p.ft = 0.0635356;
+        p.et = 2.631577918133538e-05;
+        p.rt = 1.2;
+        p.xcrp = 10000.0;
+        p.gap_close = true;
+    }
 
     ConcreteCM material(p);
     auto state = material.initial_state();
